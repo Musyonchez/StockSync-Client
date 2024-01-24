5214de4b7276d1f599f9d80b2d43d03cef545d0f
Musyonchez <musyonchez@gmail.com>
2024-01-25 00:30:12 +0300

    login form refinement

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -45,55 +45,54 @@ const LoginPage: React.FC = () => {
         setError("Invalid credentials. Please check your email, password, and company.");
       });
   };
-  
 
   return (
     <>
       <HorizontalNavbar />
-    <div className="dark:bg-gray-800 flex flex-col items-center justify-center">
-    <form onSubmit={handleSubmit} className=" mx-auto bg-white dark:bg-gray-700 p-8 rounded-lg w-64 sm:w-96">
-      <label className="block text-sm font-medium text-gray-700 dark:text-white">
-        Email:
-        <input
-          type="text"
-          value={email}
-          required
-          onChange={(e) => setEmail(e.target.value)}
-          className="mt-1 p-2 border dark:border-gray-600 rounded-md w-full"
-        />
-      </label>
-      <br />
-      <label className="block text-sm font-medium text-gray-700 dark:text-white">
-        Company:
-        <input
-          type="text"
-          value={company}
-          required
-          onChange={(e) => setCompany(e.target.value)}
-          className="mt-1 p-2 border dark:border-gray-600 rounded-md w-full"
-        />
-      </label>
-      <br />
-      <label className="block text-sm font-medium text-gray-700 dark:text-white">
-        Password:
-        <input
-          type="password"
-          value={password}
-          required
-          onChange={(e) => setPassword(e.target.value)}
-          className="mt-1 p-2 border dark:border-gray-600 rounded-md w-full"
-        />
-      </label>
-      <br />
-      <button
-        type="submit"
-        className="mt-4 p-2 bg-emerald-500 text-white rounded-md dark:bg-emerald-700 w-full"
-      >
-        Log in
-      </button>
-    </form>
-  </div>
-  </>
+      <div className="dark:bg-gray-800 flex flex-col items-center justify-center">
+        <form onSubmit={handleSubmit} className="mx-auto bg-white dark:bg-gray-700 p-8 rounded-lg w-full sm:w-96">
+          <label className="block text-sm font-medium text-gray-700 dark:text-white">
+            Email:
+            <input
+              type="text"
+              value={email}
+              required
+              onChange={(e) => setEmail(e.target.value)}
+              className="mt-1 p-2 border dark:border-gray-600 rounded-md w-full sm:w-96"
+            />
+          </label>
+          <br />
+          <label className="block text-sm font-medium text-gray-700 dark:text-white">
+            Company:
+            <input
+              type="text"
+              value={company}
+              required
+              onChange={(e) => setCompany(e.target.value)}
+              className="mt-1 p-2 border dark:border-gray-600 rounded-md w-full sm:w-96"
+            />
+          </label>
+          <br />
+          <label className="block text-sm font-medium text-gray-700 dark:text-white">
+            Password:
+            <input
+              type="password"
+              value={password}
+              required
+              onChange={(e) => setPassword(e.target.value)}
+              className="mt-1 p-2 border dark:border-gray-600 rounded-md w-full sm:w-96"
+            />
+          </label>
+          <br />
+          <button
+            type="submit"
+            className="mt-4 p-2 bg-emerald-500 text-white rounded-md dark:bg-emerald-700 w-full sm:w-96"
+          >
+            Log in
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
